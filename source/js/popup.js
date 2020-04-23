ce1e7e4d964732f13c3b49fbb32bb3a26dd81c8b
Katerina <katerina_kw@mail.ru>
2020-04-24 01:18:19 +0300

    Главная + каталог + попап + пп

---
+++ source/js/popup.js
@@ -0,0 +1,29 @@
+var link = document.querySelector(".modal-button");
+var popup = document.querySelector(".modal-popup");
+var back = document.querySelector(".modal--back");;
+var form = popup.querySelector("form");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal--show");
+  back.classList.add("modal--show");
+});
+
+back.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal--show");
+  back.classList.remove("modal--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal--show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal--show");
+    }
+    if (back.classList.contains("modal--show")) {
+      evt.preventDefault();
+      back.classList.remove("modal--show");
+    }
+  }
+});
