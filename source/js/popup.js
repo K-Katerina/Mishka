35996258cbd0e4c45db0ce5bfb3c8505ea62a0ae
Katerina <katerina_kw@mail.ru>
2020-05-09 18:14:04 +0300

    Линтер

--- source/js/popup.js
+++ source/js/popup.js
@@ -13,13 +13,13 @@ var popup = document.querySelector(".modal-popup");
 if (popup) {
   var back = document.querySelector(".modal--back");
   var form = popup && popup.querySelector("form");
-  
+
   back.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal--show");
     back.classList.remove("modal--show");
   });
-  
+
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (popup.classList.contains("modal--show")) {
