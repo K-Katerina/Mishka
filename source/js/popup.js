c488764286b359b973bd99100c9b12975bd9c419
Katerina <katerina_kw@mail.ru>
2020-05-09 18:07:38 +0300

    Автоматизация + IE

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,31 +1,35 @@
 var linkItems = document.querySelectorAll(".modal-button");
-var popup = document.querySelector(".modal-popup");
-var back = document.querySelector(".modal--back");;
-var form = popup.querySelector("form");
-
-linkItems.forEach(function (linkItem) {
-  linkItem.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal--show");
-    back.classList.add("modal--show");
-  });
-});
-
-back.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal--show");
-  back.classList.remove("modal--show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal--show")) {
+if (linkItems) {
+  for (var i = 0; i < linkItems.length; i++) {
+    linkItems[i].addEventListener("click", function (evt) {
       evt.preventDefault();
-      popup.classList.remove("modal--show");
-    }
-    if (back.classList.contains("modal--show")) {
-      evt.preventDefault();
-      back.classList.remove("modal--show");
-    }
+      popup.classList.add("modal--show");
+      back.classList.add("modal--show");
+    });
   }
-});
+}
+
+var popup = document.querySelector(".modal-popup");
+if (popup) {
+  var back = document.querySelector(".modal--back");
+  var form = popup && popup.querySelector("form");
+  
+  back.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal--show");
+    back.classList.remove("modal--show");
+  });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal--show")) {
+        evt.preventDefault();
+        popup.classList.remove("modal--show");
+      }
+      if (back.classList.contains("modal--show")) {
+        evt.preventDefault();
+        back.classList.remove("modal--show");
+      }
+    }
+  });
+}
